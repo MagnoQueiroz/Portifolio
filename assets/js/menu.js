c0a1802525382a319f6e7af522bcfa249f6be428
synxther <synxther@protonmail.com>
2023-09-07 20:41:51 -0300

    new portifolio beta

---
+++ assets/js/menu.js
@@ -0,0 +1,18 @@
+
+const menu = document.querySelector(".icon-menu");
+
+const element = document.querySelector("nav");
+const close_menu = document.querySelector(".icon-close");
+
+
+
+function toggleMenu(open = false) {
+    const elementClassList = element.classList
+    //se tá aberto fecha
+    if (open == true) {
+        elementClassList.remove("open");
+    } else {
+        //se tá fechado abre
+        elementClassList.add("open");
+    }
+}
