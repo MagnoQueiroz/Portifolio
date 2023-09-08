750a2fda77416663b9819654a8bf8866257a48c9
synxther <synxther@protonmail.com>
2023-09-07 21:03:38 -0300

    new portifolio beta

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,8 +4,6 @@ const menu = document.querySelector(".icon-menu");
 const element = document.querySelector("nav");
 const close_menu = document.querySelector(".icon-close");
 
-
-
 function toggleMenu(open = false) {
     const elementClassList = element.classList
     //se tá aberto fecha
